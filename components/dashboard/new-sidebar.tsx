d56abec43477d1dc67494d254acb9c2cb15c38d9
Adnan Alam <adnank79d@gmail.com>
2026-02-05 01:20:10 +0530

    Reduce gap between navbar and sidebar (py-6 to py-4)

--- components/dashboard/new-sidebar.tsx
+++ components/dashboard/new-sidebar.tsx
@@ -77,7 +77,7 @@ export function NewSidebar({ className, isMobile = false, onNavigate }: SidebarP
             data-version="supabase-v10"
         >
             {/* Navigation */}
-            <nav className="flex-1 flex flex-col justify-between py-6">
+            <nav className="flex-1 flex flex-col justify-between py-4">
                 <div className="flex flex-col gap-6">
                     {sidebarGroups.map((group, groupIdx) => (
                         <div key={groupIdx}>
